c675afaf7e99fcc51b4560cb86e24e0c1c1c67de
Julissa Castrillón Ríos <julissacastrillon1130370@correo.itm.edu.co>
2026-04-08 23:40:20 -0500

    Proyecto inicial completo

---
+++ src/main/java/com/kazuki_turismo/api_kazukiTurismo/ApiKazukiTurismoApplication.java
@@ -0,0 +1,13 @@
+package com.kazuki_turismo.api_kazukiTurismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiKazukiTurismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiKazukiTurismoApplication.class, args);
+	}
+
+}
