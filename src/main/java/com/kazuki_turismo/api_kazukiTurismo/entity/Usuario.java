ca94ed2222253e12ecc179d3eeb7d59d75a9d1b7
Sergio Gonzalez <sergiogonzalez1130200@correo.itm.edu.co>
2026-04-09 01:01:23 -0500

    Se creo el constructor en Usuario.Java

--- src/main/java/com/kazuki_turismo/api_kazukiTurismo/entity/Usuario.java
+++ src/main/java/com/kazuki_turismo/api_kazukiTurismo/entity/Usuario.java
@@ -1,5 +1,35 @@
-package com.kazuki_turismo.api_kazukiTurismo.entity;
+package com.kazuki_turismo.api_kazukiTurismo.model;
 
 public class Usuario {
-    
+    private int idUsuario;
+    private String nombre;
+    private String correo;
+    private String contrasena;
+    private String rolUsuario;
+
+    public Usuario() {}
+
+
+    public Usuario(int idUsuario, String nombre, String correo, String contrasena, String rolUsuario) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contrasena = contrasena;
+        this.rolUsuario = rolUsuario;
+    }
+
+    public int getIdUsuario() { return idUsuario; }
+    public void setIdUsuario(int idUsuario) { this.idUsuario = idUsuario; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getCorreo() { return correo; }
+    public void setCorreo(String correo) { this.correo = correo; }
+
+    public String getContrasena() { return contrasena; }
+    public void setContrasena(String contrasena) { this.contrasena = contrasena; }
+
+    public String getRolUsuario() { return rolUsuario; }
+    public void setRolUsuario(String rolUsuario) { this.rolUsuario = rolUsuario; }
 }
