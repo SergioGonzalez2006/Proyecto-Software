5b373081343879dbd9bb25ba54e48781b4cc8b08
Sergio Gonzalez <sergiogonzalez1130200@correo.itm.edu.co>
2026-04-10 16:59:23 -0500

    Implementacion del CRUD y clase usuarioService

---
+++ src/test/java/com/kazuki_turismo/api_kazukiTurismo/ApiKazukiTurismoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kazuki_turismo.api_kazukiTurismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiKazukiTurismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
