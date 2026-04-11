811f8cd559d4918eb8aa704fb49bc4c2a7eec5ac
Sergio Gonzalez <sergiogonzalez1130200@correo.itm.edu.co>
2026-04-11 13:53:06 -0500

    cambios menores

--- src/main/java/com/kazuki_turismo/api_kazukiTurismo/ApiKazukiTurismoApplication.java
+++ src/main/java/com/kazuki_turismo/api_kazukiTurismo/ApiKazukiTurismoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) // Agrega el exclude
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ApiKazukiTurismoApplication {
 
 	public static void main(String[] args) {
