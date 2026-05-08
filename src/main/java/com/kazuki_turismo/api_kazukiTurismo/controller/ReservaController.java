ef431bcda18bcb7240b0ff44781719af73b33840
Julissa Castrillón <julissacastrillon1130370@correo.itm.edu.co>
2026-05-07 23:20:21 -0500

    Creacion de ReservaController con metodos GET y POST para persistencia

---
+++ src/main/java/com/kazuki_turismo/api_kazukiTurismo/controller/ReservaController.java
@@ -0,0 +1,26 @@
+package com.kazuki_turismo.api_kazukiTurismo.controller;
+
+import com.kazuki_turismo.api_kazukiTurismo.model.Reserva;
+import com.kazuki_turismo.api_kazukiTurismo.repository.ReservaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reservas")
+public class ReservaController {
+
+    @Autowired
+    private ReservaRepository repository;
+
+    @GetMapping
+    public List<Reserva> listar() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public Reserva crear(@RequestBody Reserva reserva) {
+        return repository.save(reserva);
+    }
+}
